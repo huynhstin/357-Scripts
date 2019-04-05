396a8a07217800cdbed0b9489cfad5cf82a74d3a
huynhstin <justinh800@gmail.com>
2019-04-04 21:30:06 -0700

    update test

--- test.sh
+++ test.sh
@@ -33,47 +33,47 @@ function run_tests {
     done
 }
 
-if [ -d $TEST_DIRECTORY ]; then 
-    mkdir -p tests
-
-    read -p "Are your test files input (1) or command line arguments (2)? " -n 1 -r
-    echo
-    let input=$REPLY
-
-    echo "-> Running reference solution... "
-    for file in $TEST_DIRECTORY/*; do 
-        echo $file
-        run_tests "expect" "$file"
-    done
-
-    echo -e "\n-> Running your solution... "
-    echo "$HOME/a.out"
-    make clean
-    make
-
-    run_tests "actual" "$HOME/a.out"
-
-    echo -e "\n-> Diff'ing outputs... "
-    for test in $HOME/tests/*.actual; do
-        DIFF_OUT="$(diff $test ${test::-7}.expect)" 
-        testname="${test##*/}"
-        if [[ "${#DIFF_OUT}" -gt 0 ]]; then # diff outputted something
-            echo "${testname::-7} failed!"
-            diff -q $test ${test::-7}.expect # show which files differed
-            echo -e "$DIFF_OUT\n" # show actual diff output
-            let fail=1
-        else
-            echo "${testname::-7} passed!"
-        fi
-    done
-
-    if [[ fail -eq 0 ]]; then
-        echo -e "All tests passed!"
-    else 
-        echo -e "At least one test failed."
-    fi
-else
+if [ ! -d $TEST_DIRECTORY ]; then
     echo "Reference solution does not exist. Exiting.... "
     exit 1
+fi 
+
+mkdir -p tests
+
+read -p "Are your test files input (1) or command line arguments (2)? " -n 1 -r
+echo
+let input=$REPLY
+
+echo "-> Running reference solution... "
+for file in $TEST_DIRECTORY/*; do 
+    echo $file
+    run_tests "expect" "$file"
+done
+
+echo -e "\n-> Running your solution... "
+echo "$HOME/a.out"
+make clean
+make
+
+run_tests "actual" "$HOME/a.out"
+
+echo -e "\n-> Diff'ing outputs... "
+for test in $HOME/tests/*.actual; do
+    DIFF_OUT="$(diff $test ${test::-7}.expect)" 
+    testname="${test##*/}"
+    if [[ "${#DIFF_OUT}" -gt 0 ]]; then # diff outputted something
+        echo "${testname::-7} failed!"
+        diff -q $test ${test::-7}.expect # show which files differed
+        echo -e "$DIFF_OUT\n" # show actual diff output
+        let fail=1
+    else
+        echo "${testname::-7} passed!"
+    fi
+done
+
+if [[ fail -eq 0 ]]; then
+    echo -e "All tests passed!"
+else 
+    echo -e "At least one test failed."
 fi
 
